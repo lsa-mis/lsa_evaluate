68d6cbfda1b5f04f547630f1961b184cb827feb0
rsmokeUM <rsmoke@umich.edu>
2024-04-16 18:37:19 -0400

    update credentials for shibboleth and fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions"} do
     delete 'sign_out', :to => 'users/sessions#destroy', :as => :destroy_user_session
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
