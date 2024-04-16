f61bb153fd97321c78304d1334f0d191facd95dc
rsmokeUM <rsmoke@umich.edu>
2024-04-16 14:59:13 -0400

    add devise configurations and migrate user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root 'static_pages#home'
 
-# Place this at the very end of the file to catch all undefined routes
-  get '*path', to: 'application#render_404', via: :all
+  get 'static_pages/home'
 
   if Rails.env.development? || Rails.env.staging?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
+
+  # Place this at the very end of the file to catch all undefined routes
+  get '*path', to: 'application#render_404', via: :all
 
 end
