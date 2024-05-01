740d2f50c8dfe36e4e4c0174e0dbb566d6bf3f03
rsmokeUM <rsmoke@umich.edu>
2024-05-01 12:42:37 -0400

    Add connect() method to dropdown_controller.js

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,9 +1,12 @@
 // app/javascript/controllers/dropdown_controller.js
 import { Controller } from "@hotwired/stimulus"
-
 export default class extends Controller {
   static targets = ["menu"]
 
+  connect() {
+    console.log("Dropdown controller connected");
+  }
+  
   toggle(event) {
     this.menuTarget.classList.toggle('show');  // Toggle the 'show' class on the dropdown menu
     event.stopPropagation();  // Prevent the event from propagating to the document
