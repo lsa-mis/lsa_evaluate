96613b5b04c416d188602125869389db73b96890
rsmokeUM <rsmoke@umich.edu>
2024-05-23 10:39:17 -0400

    feat: Refactor error handling in ApplicationController
    This commit refactors the error handling in the ApplicationController by renaming the `render_404` and `render_500` methods to `render404` and `render500` respectively. This change improves code readability and follows the established convention of using camel case for method names.
    
    Co-authored-by: dependabot[bot] <support@github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from Exception, with: :render_500
+  rescue_from ActiveRecord::RecordNotFound, with: :render404
+  rescue_from Exception, with: :render500
 
-  def render_404
+  def render404
     respond_to do |format|
       format.html { render 'errors/not_found', status: :not_found, layout: 'application' }
       format.json { render json: { error: 'Not Found' }, status: :not_found }
     end
   end
 
-  def render_500(_exception)
+  def render500(_exception)
     # Log the error, send it to error tracking services, etc.
     respond_to do |format|
       format.html { render 'errors/internal_server_error', status: :internal_server_error, layout: 'application' }
