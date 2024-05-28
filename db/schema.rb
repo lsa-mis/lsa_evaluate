555f2b46aa163b0782d38d8d242a948f08e3cda6
rsmokeUM <rsmoke@umich.edu>
2024-05-28 11:51:58 -0400

    Update user table schema to use utf8mb4 charset and utf8mb4_unicode_ci collation

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_18_141957) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
