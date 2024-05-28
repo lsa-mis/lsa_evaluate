7250225c0a7a33d69e0ea4503709f1b3e3da1866
rsmokeUM <rsmoke@umich.edu>
2024-05-28 11:52:24 -0400

    Update user table schema to use utf8mb4 charset and utf8mb4_unicode_ci collation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,24 +6,24 @@
 #
 #  id                     :bigint           not null, primary key
 #  current_sign_in_at     :datetime
-#  current_sign_in_ip     :string
-#  display_name           :string
-#  email                  :string           default(""), not null
-#  encrypted_password     :string           default(""), not null
+#  current_sign_in_ip     :string(255)
+#  display_name           :string(255)
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
 #  failed_attempts        :integer          default(0), not null
 #  last_sign_in_at        :datetime
-#  last_sign_in_ip        :string
+#  last_sign_in_ip        :string(255)
 #  locked_at              :datetime
-#  person_affiliation     :string
-#  principal_name         :string
-#  provider               :string
+#  person_affiliation     :string(255)
+#  principal_name         :string(255)
+#  provider               :string(255)
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
-#  reset_password_token   :string
+#  reset_password_token   :string(255)
 #  sign_in_count          :integer          default(0), not null
-#  uid                    :string
-#  uniqname               :string
-#  unlock_token           :string
+#  uid                    :string(255)
+#  uniqname               :string(255)
+#  unlock_token           :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
