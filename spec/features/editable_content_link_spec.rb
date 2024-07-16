4f892b6bcc6ec8f47c954c2232373bf6e628a97c
rsmokeUM <rsmoke@umich.edu>
2024-07-16 10:06:50 -0400

    editable_content_link_spec.rb
    This commit refactors the 'editable_content_link_spec.rb' file to improve the display of the edit link with a pencil icon. It updates the CSS selector for the pencil icon and adds assertions to ensure that the link and icon are correctly displayed. This refactor enhances the clarity and maintainability of the code related to the edit link in the editable content feature.
    
    Co-authored-by: dependabot[bot] <support@github.com>

--- spec/features/editable_content_link_spec.rb
+++ spec/features/editable_content_link_spec.rb
@@ -2,23 +2,37 @@
 
 require 'rails_helper'
 
-RSpec.describe 'EditableContent' do
+RSpec.describe 'EditableContent', type: :feature do
   let!(:description) do
     create(:editable_content, page: 'home', section: 'description',
                               content: 'A short paragraph explaining LSA Evaluate.')
   end
-  let(:user) { create(:user) } # Assuming you have a user factory
+  let(:user) { create(:user) }
+  let(:admin) { create(:user) }
+  let!(:role) { create(:role, kind: 'editable_content_administrator') }
+  let!(:user_role) { create(:user_role, user: admin, role:) }
 
-  context 'when user is logged in' do
+  context 'when editable_content_administrator is logged in' do
+    before do
+      login_as(admin)
+      visit root_path
+    end
+
+    it 'displays the edit link with a pencil icon' do
+      expect(page).to have_link('', href: editable_content_path(description))
+      expect(page).to have_css('a[href="' + editable_content_path(description) + '"] .bi.bi-pencil')
+    end
+  end
+
+  context 'when NON editable_content_administrator is logged in' do
     before do
       login_as(user)
       visit root_path
     end
 
-    it 'displays the edit link with a pencil icon' do
-      expect(page).to have_link(href: editable_content_path(description)) do |link|
-        expect(link).to have_css('bi bi-pencil')
-      end
+    it 'does not display the edit link with a pencil icon' do
+      expect(page).to have_no_link('', href: editable_content_path(description))
+      expect(page).to have_no_css('a[href="' + editable_content_path(description) + '"] .bi.bi-pencil')
     end
   end
 
@@ -28,8 +42,8 @@ RSpec.describe 'EditableContent' do
     end
 
     it 'does not display the edit link with a pencil icon' do
-      expect(page).to have_no_link(href: editable_content_path(description))
-      expect(page).to have_no_css('bi bi-pencil')
+      expect(page).to have_no_link('', href: editable_content_path(description))
+      expect(page).to have_no_css('.bi.bi-pencil')
     end
   end
 end
