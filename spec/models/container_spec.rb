66c34135289b4cfd3755f2d5f1ed7e9602cbfb20
rsmokeUM <rsmoke@umich.edu>
2024-07-17 11:22:57 -0400

    feat: Add associations, validations, and indexes to Container model
    This commit adds associations, validations, and indexes to the `Container` model. It includes the following changes:
    - Added associations with `department`, `visibility`, `assignments`, `users`, `roles`, and `contest_descriptions`
    - Added nested attributes for `assignments` and `contest_descriptions`
    - Added validations for `name`, `department_id`, and `visibility_id`
    - Added indexes for `department_id` and `visibility_id`
    
    Co-authored-by: dependabot[bot] <support@github.com>

--- spec/models/container_spec.rb
+++ spec/models/container_spec.rb
@@ -22,6 +22,90 @@
 #
 require 'rails_helper'
 
-RSpec.describe Container do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Container, type: :model do
+  describe 'associations' do
+    it 'belongs to department' do
+      association = described_class.reflect_on_association(:department)
+      expect(association.macro).to eq :belongs_to
+    end
+
+    it 'belongs to visibility' do
+      association = described_class.reflect_on_association(:visibility)
+      expect(association.macro).to eq :belongs_to
+    end
+
+    it 'has many assignments with dependent destroy' do
+      association = described_class.reflect_on_association(:assignments)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
+    end
+
+    it 'has many users through assignments' do
+      association = described_class.reflect_on_association(:users)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :assignments
+    end
+
+    it 'has many roles through assignments' do
+      association = described_class.reflect_on_association(:roles)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:through]).to eq :assignments
+    end
+
+    it 'has many contest_descriptions with dependent destroy' do
+      association = described_class.reflect_on_association(:contest_descriptions)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
+    end
+  end
+
+  describe 'nested attributes' do
+    it 'accepts nested attributes for assignments with allow destroy' do
+      expect(Container.nested_attributes_options[:assignments][:allow_destroy]).to be true
+    end
+
+    it 'accepts nested attributes for contest_descriptions with allow destroy' do
+      expect(Container.nested_attributes_options[:contest_descriptions][:allow_destroy]).to be true
+    end
+  end
+
+  describe 'validations' do
+    it 'validates presence of name' do
+      container = Container.new(name: nil)
+      container.validate
+      expect(container).not_to be_valid
+    end
+
+    it 'validates presence of department_id' do
+      container = Container.new(department_id: nil)
+      container.validate
+      expect(container).not_to be_valid
+    end
+
+    it 'validates presence of visibility_id' do
+      container = Container.new(visibility_id: nil)
+      container.validate
+      expect(container).not_to be_valid
+    end
+  end
+
+  describe 'indexes' do
+    it 'has index on department_id' do
+      expect(ActiveRecord::Base.connection.index_exists?(:containers, :department_id)).to be true
+    end
+
+    it 'has index on visibility_id' do
+      expect(ActiveRecord::Base.connection.index_exists?(:containers, :visibility_id)).to be true
+    end
+  end
+
+  describe 'foreign keys' do
+    it 'has foreign key on department_id' do
+      expect(Container.connection.foreign_keys('containers').any? { |fk| fk.column == 'department_id' }).to be true
+    end
+
+    it 'has foreign key on visibility_id' do
+      expect(Container.connection.foreign_keys('containers').any? { |fk| fk.column == 'visibility_id' }).to be true
+    end
+  end
 end
