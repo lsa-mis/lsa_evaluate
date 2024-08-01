80378e16e4770e7498bee0933a7d2c1aa6f95730
rsmokeUM <rsmoke@umich.edu>
2024-08-01 09:28:59 -0400

    add error handlng to leverage CanCan auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from ActiveRecord::RecordNotFound, with: :render404
-  # rescue_from StandardError, with: :render500
+  rescue_from StandardError, with: :render500
 
   def render404
     respond_to do |format|
@@ -24,11 +24,19 @@ class ApplicationController < ActionController::Base
 
   def render500(exception)
     # Log the error, send it to error tracking services, etc.
-    logger.error(exception.message)
+    logger.error("!!!!!!!!!!! StandardError: #{exception.message}")
     logger.error(exception.backtrace.join("\n"))
-    respond_to do |format|
-      format.html { render 'errors/internal_server_error', status: :internal_server_error, layout: 'application' }
-      format.json { render json: { error: 'Internal Server Error' }, status: :internal_server_error }
+
+    if exception.message.include?('You are not authorized')
+      respond_to do |format|
+        format.html { redirect_to root_path, alert: 'You are not authorized for that action' }
+        format.json { render json: { error: 'You are not authorized for that action' }, status: :forbidden }
+      end
+    else
+      respond_to do |format|
+        format.html { render 'errors/internal_server_error', status: :internal_server_error, layout: 'application' }
+        format.json { render json: { error: 'Internal Server Error' }, status: :internal_server_error }
+      end
     end
   end
 end
