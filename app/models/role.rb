caa20f4bfae6c8720742fef5d834f0e57024ddfd
rsmokeUM <rsmoke@umich.edu>
2024-08-13 10:12:02 -0400

    feat: Add display_name method to Role model
    The code changes add a `display_name` method to the `Role` model. This method returns the `kind` attribute of the role, which will be used to display the role's name in the user interface. This change enhances the user experience by providing a more meaningful representation of the role.

--- app/models/role.rb
+++ app/models/role.rb
@@ -16,4 +16,8 @@ class Role < ApplicationRecord
   has_many :user_roles, dependent: :destroy
   has_many :users, through: :user_roles
   has_many :assignments, dependent: :destroy
+
+  def display_name
+    kind
+  end
 end
