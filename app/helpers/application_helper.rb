fffa73efa0dbb780d71b9943b63848217d525621
Margarita Barvinok <brita@umich.edu>
2024-08-14 18:14:12 -0400

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
     content_html = render_rich_text_without_outer_div(content.content)
     if current_user&.axis_mundi?
-      edit_link = link_to(editable_content_path(content), class: 'edit-link ms-2') do
+      edit_link = link_to(edit_editable_content_path(content), class: 'edit-link ms-2') do
         content_tag(:i, '', class: 'bi bi-pencil')
       end
       content_html += edit_link
@@ -22,4 +22,16 @@ module ApplicationHelper
   def format_datetime(datetime)
     datetime.strftime('%m/%d/%Y %I:%M %p')
   end
+
+  def redirect_back_or_default(notice: '', alert: false, default: root_url)
+    if alert
+      flash[:alert] = notice
+    else
+      flash[:notice] = notice
+    end
+    url = session[:return_to]
+    session[:return_to] = nil
+    redirect_to(url, anchor: "top" || default)
+  end
+
 end
