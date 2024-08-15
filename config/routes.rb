a4c09c9ad17e7f094a2459cbddead3586bb49928
rsmokeUM <rsmoke@umich.edu>
2024-08-15 19:44:28 -0400

    refactor: Update editable_contents_controller.rb
    Refactor `editable_contents_controller.rb` to remove the `show` action and associated routes. This improves code organization and removes unnecessary functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
       get 'entrant_content'
     end
   end
-  resources :editable_contents, only: %i[index show edit update]
+  resources :editable_contents, only: %i[index edit update]
   resources :statuses
   resources :categories
   resources :contest_descriptions
