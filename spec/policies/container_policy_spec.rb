16e3a1bebb086224f8b17ea1f5b671935a26c908
rsmokeUM <rsmoke@umich.edu>
2024-08-20 14:11:21 -0400

    refactor: Update ContainerPolicy for index and show actions

--- spec/policies/container_policy_spec.rb
+++ spec/policies/container_policy_spec.rb
@@ -3,13 +3,17 @@ require 'rails_helper'
 RSpec.describe ContainerPolicy do
   subject { described_class.new(user, container) }
 
-  let(:container) { create(:container) }
-  let(:axis_mundi_role) { create(:role, kind: 'Axis Mundi') }
+  # Create the container before each test
+  let!(:container) { create(:container) } # Ensure container is saved to the database
+  let!(:axis_mundi_role) { create(:role, kind: 'Axis Mundi') }
+  let!(:container_admin_role) { create(:role, kind: 'Container Administrator') }
+  let!(:container_manager_role) { create(:role, kind: 'Container Manager') }
   let(:axis_mundi_user) { create(:user) }
   let(:creator) { create(:user, person_affiliation: 'employee') }
 
   before do
-    create(:assignment, container: container, user: creator)
+    # Ensure the roles and assignments are created after the container is available
+    create(:assignment, container: container, user: creator, role: container_admin_role)
   end
 
   context 'for an employee user' do
@@ -26,11 +30,15 @@ RSpec.describe ContainerPolicy do
     it { is_expected.not_to permit_action(:update) }
   end
 
-  context 'for an employee user who did not create the container' do
+  context 'for an employee user who did not create the container but has a manager role' do
     let(:user) { create(:user, person_affiliation: 'employee') }
 
+    before do
+      create(:assignment, container: container, user: user, role: container_manager_role)
+    end
+
     it { is_expected.to permit_action(:create) }
-    it { is_expected.not_to permit_action(:update) }
+    it { is_expected.to permit_action(:update) }
   end
 
   context 'for an Axis Mundi user' do
