4c445341562503c0edb8537c3e3cedb1ea82a61e
rsmokeUM <rsmoke@umich.edu>
2024-08-20 14:10:57 -0400

    refactor: Update ContainerPolicy for index and show actions
    The code changes in the ContainerPolicy class update the authorization logic for the index and show actions. The updated logic now checks if the user has assignment roles or is an admin user. This ensures that only users with assignment roles or admin users can access the index and show actions, improving the security and access control of the application.

--- app/policies/container_policy.rb
+++ app/policies/container_policy.rb
@@ -1,27 +1,55 @@
 class ContainerPolicy < ApplicationPolicy
+  class Scope < ApplicationPolicy::Scope
+    private
+
+    def specific_scope
+      scope.joins(:assignments).where(assignments: { user_id: user.id, role_id: [ container_admin_role.id, container_manager_role.id ] })
+    end
+
+    def container_admin_role
+      @container_admin_role ||= Role.find_by(kind: 'Container Administrator')
+    end
+
+    def container_manager_role
+      @container_manager_role ||= Role.find_by(kind: 'Container Manager')
+    end
+  end
+
+  def index?
+    user_has_assignment_role? || admin_user?
+  end
+
+  def show?
+    user_has_assignment_role? || admin_user?
+  end
+
   def create?
     user_is_employee? || admin_user?
   end
 
   def update?
-    user_is_creator? || admin_user?
+    user_has_assignment_role? || admin_user?
   end
 
   def destroy?
-    user_is_creator? || admin_user?
+    user_has_assignment_role? || admin_user?
   end
 
   private
 
-  def user_is_creator?
-    record.assignments.exists?(user: user)
+  def user_has_assignment_role?
+    record.assignments.exists?(user_id: user.id, role_id: [ container_admin_role.id, container_manager_role.id ])
   end
 
   def user_is_employee?
     user.person_affiliation == 'employee'
   end
 
-  def admin_user?
-    user.user_roles.joins(:role).exists?(roles: { kind: 'Axis Mundi' })
+  def container_admin_role
+    Role.find_by(kind: 'Container Administrator')
+  end
+
+  def container_manager_role
+    Role.find_by(kind: 'Container Manager')
   end
 end
