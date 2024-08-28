8ae25240f1b33c23c60cef2668a54ad0f391ca89
rsmokeUM <rsmoke@umich.edu>
2024-08-28 11:57:25 -0400

    refactor: Update routes.rb to redirect all routes to maintenance page in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,53 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :entries
-  get 'applicant_dashboard/index'
-  resources :testingrsmokes
-  root 'static_pages#home'
+  if Rails.env.production?
+    # Redirect all routes to the maintenance page in production
+    match '*path', to: redirect('/maintenance.html'), via: :all
+    root to: redirect('/maintenance.html')
+  else
+    root 'static_pages#home'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' } do
-    delete 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
-  end
+    resources :entries
+    get 'applicant_dashboard/index'
+    resources :testingrsmokes
 
-  resources :containers do
-    resources :contest_descriptions do
-      resources :contest_instances, path: 'instances'
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' } do
+      delete 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
     end
-    resources :assignments, only: %i[create destroy]
-  end
-  resources :visibilities
-  resources :departments
-  resources :roles
-  resources :static_pages do
-    collection do
-      get 'entrant_content'
+
+    resources :containers do
+      resources :contest_descriptions do
+        resources :contest_instances, path: 'instances'
+      end
+      resources :assignments, only: %i[create destroy]
     end
+
+    resources :visibilities
+    resources :departments
+    resources :roles
+    resources :static_pages do
+      collection do
+        get 'entrant_content'
+      end
+    end
+    resources :editable_contents, only: %i[index edit update]
+    resources :statuses
+    resources :categories
+    # resources :contest_descriptions
+    resources :class_levels
+    # resources :contest_instances
+    resources :address_types
+    resources :campuses
+    resources :schools
+    resources :addresses
+    resources :profiles, only: %i[index show new create edit update destroy]
+    resources :user_roles
+    get 'applicant_dashboard', to: 'applicant_dashboard#index'
+
+    mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development? || Rails.env.staging?
+
+    # Place this at the very end of the file to catch all undefined routes
+    get '*path', to: 'application#render404', via: :all
   end
-  resources :editable_contents, only: %i[index edit update]
-  resources :statuses
-  resources :categories
-  # resources :contest_descriptions
-  resources :class_levels
-  # resources :contest_instances
-  resources :address_types
-  resources :campuses
-  resources :schools
-  resources :addresses
-  resources :profiles, only: %i[index show new create edit update destroy]
-  resources :user_roles
-  get 'applicant_dashboard', to: 'applicant_dashboard#index'
-
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development? || Rails.env.staging?
-
-  # Place this at the very end of the file to catch all undefined routes
-  get '*path', to: 'application#render404', via: :all
 end
