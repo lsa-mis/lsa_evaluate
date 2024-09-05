c2c062b97e2efb114d3337f7e740ac882e50de5f
rsmokeUM <rsmoke@umich.edu>
2024-09-05 17:48:14 -0400

    refactor: Update affiliation_spec.rb to include validations and associations

--- spec/models/affiliation_spec.rb
+++ spec/models/affiliation_spec.rb
@@ -1,23 +1,41 @@
-# == Schema Information
-#
-# Table name: affiliations
-#
-#  id         :bigint           not null, primary key
-#  name       :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
-#
-# Indexes
-#
-#  index_affiliations_on_user_id  (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
-#
+# spec/models/affiliation_spec.rb
+
 require 'rails_helper'
 
 RSpec.describe Affiliation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      affiliation = build(:affiliation, user: user)
+      expect(affiliation).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      affiliation = build(:affiliation, name: nil, user: user)
+      expect(affiliation).not_to be_valid
+    end
+
+    it 'is not valid without a user' do
+      affiliation = build(:affiliation, user: nil)
+      expect(affiliation).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs to a user' do
+      association = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq(:belongs_to)
+    end
+  end
+
+  describe 'database structure' do
+    it 'has a name column' do
+      expect(Affiliation.column_names).to include('name')
+    end
+
+    it 'has a user_id column' do
+      expect(Affiliation.column_names).to include('user_id')
+    end
+  end
 end
