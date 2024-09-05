38b497e8c0b161fbae33f3d697ac0b2210ecbf2e
rsmokeUM <rsmoke@umich.edu>
2024-09-05 17:28:46 -0400

    feat: Update user factory to include affiliations and improve data generation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -34,9 +34,30 @@
 #
 FactoryBot.define do
   factory :user do
-    uniqname { Faker::String.random(length: 3..8) }
     email { Faker::Internet.email }
+    # password { "passwordpassword" }
     password { Faker::Internet.password(min_length: 15) }
+    uniqname { Faker::Internet.username }
+    uid { uniqname }
+    principal_name { email }
     display_name { Faker::Name.name }
+
+    trait :employee do
+      after(:create) do |user|
+        create(:affiliation, user: user, name: 'employee')
+      end
+    end
+
+    trait :student do
+      after(:create) do |user|
+        create(:affiliation, user: user, name: 'student')
+      end
+    end
+
+    trait :faculty do
+      after(:create) do |user|
+        create(:affiliation, user: user, name: 'faculty')
+      end
+    end
   end
 end
