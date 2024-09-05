2bdd8c8755e40c08bce240c14c45fd2af63abf46
rsmokeUM <rsmoke@umich.edu>
2024-09-05 17:30:37 -0400

    feat: Add user avatar display in shibboleth login spec

--- spec/system/shib_login_spec.rb
+++ spec/system/shib_login_spec.rb
@@ -1,33 +1,37 @@
 # frozen_string_literal: true
 
-# spec/system/shib_login_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe 'Shibboleth', type: :request do
+  let(:user) { create(:user, :employee) }
+
   describe 'login success -' do
-    it 'displays user avatar' do
-      user = create(:user)
+    before do
       mock_login({
                    email: user.email,
                    name: user.display_name,
                    uniqname: user.uniqname
                  })
-      raise "Expected a redirect but got #{response.status}" unless response.redirect?
+    end
 
+    it 'displays user avatar' do
+      expect(response).to be_redirect
       follow_redirect!
       expect(response.body).to include('id="avatar"')
+      expect(response.body).to include(user.display_name)
     end
   end
 
   describe 'login failure -' do
-    it 'does not displays user avatar' do
-      user = create(:user)
+    before do
       mock_login({
                    email: 'kielbasa',
                    name: user.display_name,
                    uniqname: user.uniqname
                  })
+    end
+
+    it 'does not display user avatar' do
       expect(response.body).not_to include('id="avatar"')
     end
   end
