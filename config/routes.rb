edc6467d047b4aa3bcb5b5f55b55ffd8828d7d55
Margarita Barvinok <brita@umich.edu>
2024-09-05 11:27:26 -0400

    fix routes after rebase

--- config/routes.rb
+++ config/routes.rb
@@ -7,46 +7,8 @@ Rails.application.routes.draw do
   get 'applicant_dashboard/index'
   resources :testingrsmokes
 
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' } do
-      delete 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
-    end
-
-    resources :containers do
-      resources :contest_descriptions do
-        resources :contest_instances, path: 'instances'
-      end
-      resources :assignments, only: %i[create destroy]
-    end
-
-    get '/containers/:container_id/contest_descriptions_for_container/', to: 'contest_descriptions#contest_descriptions_for_container', as: :contest_descriptions_for_container
-    post '/containers/:container_id/contest_descriptions/create_instances_for_selected_descriptions', to: 'contest_instances#create_instances_for_selected_descriptions', as: :create_instances_for_selected_descriptions
-
-    resources :visibilities
-    resources :departments
-    resources :roles
-    resources :static_pages do
-      collection do
-        get 'entrant_content'
-      end
-    end
-    resources :editable_contents, only: %i[index edit update]
-    resources :statuses
-    resources :categories
-    # resources :contest_descriptions
-    resources :class_levels
-    # resources :contest_instances
-    resources :address_types
-    resources :campuses
-    resources :schools
-    resources :addresses
-    resources :profiles, only: %i[index show new create edit update destroy]
-    resources :user_roles
-    get 'applicant_dashboard', to: 'applicant_dashboard#index'
-
-    mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development? || Rails.env.staging?
-
-    # Place this at the very end of the file to catch all undefined routes
-    get '*path', to: 'application#render404', via: :all
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' } do
+    delete 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
 
   resources :containers do
