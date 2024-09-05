a1afe41f066d9299311702e82a5c8d59c460b511
rsmokeUM <rsmoke@umich.edu>
2024-09-05 17:43:49 -0400

    refactor: Remove unused person_affiliation column from users table

---
+++ spec/models/affiliation_spec.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: affiliations
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_affiliations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+require 'rails_helper'
+
+RSpec.describe Affiliation, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
