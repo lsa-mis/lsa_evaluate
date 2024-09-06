1e8353aaadd61e02777f7683ed6b5c7dc0df057d
rsmokeUM <rsmoke@umich.edu>
2024-09-05 20:09:42 -0400

    refactor: Update affiliations.rb factory to include schema information
    This commit updates the affiliations.rb factory in the spec/factories/affiliations.rb file to include the schema information for the affiliations table. By adding the schema information, it provides a clear understanding of the table structure and its indexes and foreign keys. This change improves code readability and documentation for the affiliations factory.

--- spec/models/affiliation_spec.rb
+++ spec/models/affiliation_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: affiliations
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_affiliations_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 # spec/models/affiliation_spec.rb
 
 require 'rails_helper'
