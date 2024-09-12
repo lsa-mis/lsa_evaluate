06c2057f8219d95c4722e2bf7424aaeb3f693f15
rsmokeUM <rsmoke@umich.edu>
2024-09-12 13:43:36 -0400

    Refactor rails_helper.rb to include Pundit matchers for policy testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'rspec/rails'
 require 'factory_bot_rails'
+require 'pundit/rspec'
 
 puts "!*!*!*! Running in environment: #{Rails.env} !*!*!*!"
 
@@ -27,6 +28,8 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.include Pundit::Matchers, type: :policy
 
   config.fixture_paths = [ Rails.root.join('spec/fixtures').to_s ]
 
