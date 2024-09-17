30542f4d89de03a7780660a15406d61c0714882b
rsmokeUM <rsmoke@umich.edu>
2024-09-17 16:23:05 -0400

    Refactor scroll_controller.js to add console log for Stimulus initialization

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -7,6 +7,7 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log("Hello, Stimulus!", this.scrollToTopValue)
     if (this.scrollToTopValue) {
       window.scrollTo(0, 0)
     }
