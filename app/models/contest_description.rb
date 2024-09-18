17f3a4a459efd9c067e366f7a4caa181a6b81182
rsmokeUM <rsmoke@umich.edu>
2024-09-18 19:51:30 -0400

    Refactor ContestDescription model to remove unused columns and add rich text fields for eligibility rules and notes

--- app/models/contest_description.rb
+++ app/models/contest_description.rb
@@ -2,17 +2,15 @@
 #
 # Table name: contest_descriptions
 #
-#  id                :bigint           not null, primary key
-#  active            :boolean          default(FALSE), not null
-#  archived          :boolean          default(FALSE), not null
-#  created_by        :string(255)      not null
-#  eligibility_rules :text(65535)
-#  name              :string(255)      not null
-#  notes             :text(65535)
-#  short_name        :string(255)
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  container_id      :bigint           not null
+#  id           :bigint           not null, primary key
+#  active       :boolean          default(FALSE), not null
+#  archived     :boolean          default(FALSE), not null
+#  created_by   :string(255)      not null
+#  name         :string(255)      not null
+#  short_name   :string(255)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  container_id :bigint           not null
 #
 # Indexes
 #
@@ -25,6 +23,9 @@
 class ContestDescription < ApplicationRecord
   belongs_to :container
   has_many :contest_instances, dependent: :destroy
+
+  has_rich_text :eligibility_rules
+  has_rich_text :notes
 
   accepts_nested_attributes_for :contest_instances, allow_destroy: true
 
