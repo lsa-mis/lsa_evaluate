a3aaff963485a72080490ce83f86d59b3424d0d4
rsmokeUM <rsmoke@umich.edu>
2024-09-18 19:52:23 -0400

    Remove eligibility_rules and notes columns from ContestDescription model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_10_211246) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_18_224440) do
   create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
@@ -155,8 +155,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_10_211246) do
     t.bigint "container_id", null: false
     t.string "name", null: false
     t.string "short_name"
-    t.text "eligibility_rules"
-    t.text "notes"
     t.string "created_by", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
