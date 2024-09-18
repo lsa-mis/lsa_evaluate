0d00024b6814bddcfdea75630e81b85cab6bd33e
rsmokeUM <rsmoke@umich.edu>
2024-09-18 17:37:30 -0400

    Refactor entries_controller_spec.rb to add soft_delete action tests

---
+++ spec/controllers/entries_controller_spec.rb
@@ -0,0 +1,41 @@
+# spec/controllers/entries_controller_spec.rb
+require 'rails_helper'
+
+RSpec.describe EntriesController, type: :controller do
+  describe 'PATCH #soft_delete' do
+    let(:user) { create(:user) }
+    let(:profile) { create(:profile, user: user) }
+    let(:contest_instance) { create(:contest_instance, date_closed: date_closed) }
+    let(:entry) { create(:entry, profile: profile, contest_instance: contest_instance) }
+
+    before do
+      sign_in user
+    end
+
+    context 'when the contest instance is still open (date_closed in the future)' do
+      let(:date_closed) { 1.day.from_now }
+
+      it 'soft deletes the entry and redirects with a success notice' do
+        patch :soft_delete, params: { id: entry.id }
+
+        entry.reload
+        expect(entry.deleted).to be true
+        expect(response).to redirect_to(applicant_dashboard_path)
+        expect(flash[:notice]).to eq('Entry was successfully removed.')
+      end
+    end
+
+    context 'when the contest instance has closed (date_closed in the past)' do
+      let(:date_closed) { 1.day.ago }
+
+      it 'does not delete the entry and redirects with an alert' do
+        patch :soft_delete, params: { id: entry.id }
+
+        entry.reload
+        expect(entry.deleted).to be false
+        expect(response).to redirect_to(applicant_dashboard_path)
+        expect(flash[:alert]).to eq('Cannot delete entry after contest has closed.')
+      end
+    end
+  end
+end
