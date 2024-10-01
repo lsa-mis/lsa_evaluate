4be839c56c53404634f5cce17f7beca527889442
rsmokeUM <rsmoke@umich.edu>
2024-10-01 08:27:28 -0400

    Refactor ContestDescriptionsController: Update create, update, and destroy actions

--- app/controllers/contest_descriptions_controller.rb
+++ app/controllers/contest_descriptions_controller.rb
@@ -2,8 +2,10 @@ class ContestDescriptionsController < ApplicationController
   before_action :set_container
   before_action :set_contest_description, only: %i[show edit update destroy eligibility_rules]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
-    @contest_descriptions = ContestDescription.all
+    @contest_descriptions = @container.contest_descriptions.all
   end
 
   def show; end
@@ -16,56 +18,11 @@ class ContestDescriptionsController < ApplicationController
 
   def create
     @contest_description = @container.contest_descriptions.new(contest_description_params)
-
-    respond_to do |format|
-      if @contest_description.save
-        format.turbo_stream {
-                              redirect_to container_path(@container),
-                              notice: I18n.t('notices.contest_description.create')
-                            }
-        format.html {
-                      redirect_to container_path(@container),
-                      notice: I18n.t('notices.contest_description.create')
-                    }
-      else
-        format.turbo_stream {
-                              render turbo_stream: turbo_stream.replace('contest_description_form',
-                              partial: 'contest_descriptions/form',
-                              locals: { contest_description: @contest_description }),
-                              status: :unprocessable_entity
-                            }
-        format.html {
-                      render :new,
-                      status: :unprocessable_entity
-                    }
-      end
-    end
+    handle_save(@contest_description.save, 'created')
   end
 
   def update
-    respond_to do |format|
-      if @contest_description.update(contest_description_params)
-        format.turbo_stream {
-                              redirect_to container_path(@container),
-                              notice: I18n.t('notices.contest_description.updated.')
-                            }
-        format.html {
-                      redirect_to container_path(@container),
-                      notice: I18n.t('notices.contest_description.updated.')
-                    }
-      else
-        format.turbo_stream {
-          render turbo_stream: turbo_stream.replace('contest_description_form',
-          partial: 'contest_descriptions/form',
-          locals: { contest_description: @contest_description }),
-          status: :unprocessable_entity
-        }
-        format.html {
-          render :edit,
-          status: :unprocessable_entity
-        }
-      end
-    end
+    handle_save(@contest_description.update(contest_description_params), 'updated')
   end
 
   def contest_descriptions_for_container
@@ -73,14 +30,16 @@ class ContestDescriptionsController < ApplicationController
   end
 
   def destroy
-    @contest_description.destroy
-
-    respond_to do |format|
-      format.turbo_stream
-      format.html {
-        redirect_to containers_path,
-        notice: I18n.t('notices.contest_description.destroyed')
-      }
+    if @contest_description.destroy
+      respond_to do |format|
+        format.turbo_stream { redirect_to containers_path, notice: I18n.t('notices.contest_description.destroyed') }
+        format.html { redirect_to containers_path, notice: I18n.t('notices.contest_description.destroyed') }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { redirect_to containers_path, alert: @contest_description.errors.full_messages.to_sentence }
+        format.html { redirect_to containers_path, alert: @contest_description.errors.full_messages.to_sentence }
+      end
     end
   end
 
@@ -95,6 +54,21 @@ class ContestDescriptionsController < ApplicationController
   end
 
   private
+
+  def handle_save(success, action)
+    respond_to do |format|
+      if success
+        format.turbo_stream { redirect_to container_path(@container), notice: I18n.t("notices.contest_description.#{action}") }
+        format.html { redirect_to container_path(@container), notice: I18n.t("notices.contest_description.#{action}") }
+      else
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace('contest_description_form', partial: 'contest_descriptions/form', locals: { contest_description: @contest_description }), status: :unprocessable_entity
+        }
+        format.html { render action_name.to_sym, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def set_container
     @container = Container.find(params[:container_id])
   end
@@ -107,6 +81,6 @@ class ContestDescriptionsController < ApplicationController
     params.require(:contest_description).permit(:created_by, :active, :archived,
                                                 :eligibility_rules, :name, :notes,
                                                 :short_name,
-                                                :container_id, :status_id)
+                                                :container_id)
   end
 end
