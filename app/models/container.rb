3e7f8279dd023286a3c4d1f28d73a901a87f02b7
rsmokeUM <rsmoke@umich.edu>
2024-10-01 12:52:19 -0400

    Refactor containers table: Rename description column to notes

--- app/models/container.rb
+++ app/models/container.rb
@@ -5,8 +5,8 @@
 # Table name: containers
 #
 #  id            :bigint           not null, primary key
-#  description   :text(65535)
 #  name          :string(255)
+#  notes         :text(65535)
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  department_id :bigint           not null
@@ -30,6 +30,8 @@ class Container < ApplicationRecord
   has_many :roles, through: :assignments
   has_many :contest_descriptions, dependent: :restrict_with_error
 
+  has_rich_text :description
+
   attr_accessor :creator
 
   accepts_nested_attributes_for :assignments, allow_destroy: true, reject_if: :all_blank
@@ -46,15 +48,15 @@ class Container < ApplicationRecord
 
   def assign_container_administrator
     return if creator.blank?  # Skip if creator is nil
-    container_admin_role = Role.find_by(kind: 'Container Administrator')
+    container_admin_role = Role.find_by(kind: 'Collection Administrator')
 
     if container_admin_role.present?
       assignment = assignments.create(user: creator, role: container_admin_role)
       unless assignment.persisted?
-        raise ActiveRecord::Rollback, 'Container Administrator assignment failed to be created'
+        raise ActiveRecord::Rollback, 'Collection Administrator assignment failed to be created'
       end
     else
-      errors.add(:base, 'Container Administrator role not found.')
+      errors.add(:base, 'Collection Administrator role not found.')
       raise ActiveRecord::Rollback
     end
   end
