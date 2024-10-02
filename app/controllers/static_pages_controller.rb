284911e9a0bb7b0d5f99f371c0a31cdb50907e50
rsmokeUM <rsmoke@umich.edu>
2024-10-02 15:41:32 -0400

    Refactor static_pages_controller.rb: Remove unused code and update home action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,5 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home]
 
-  def home
-    @departments = Department.all
-    @class_levels = ClassLevel.all
-    @containers = Container.all
-  end
+  def home; end
 end
