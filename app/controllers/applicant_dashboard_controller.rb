c2743503c642b9f93d95e7a71a4d7595e09b5292
rsmokeUM <rsmoke@umich.edu>
2024-10-02 14:17:13 -0400

    Refactor applicant dashboard view: Update available_contests method

--- app/controllers/applicant_dashboard_controller.rb
+++ app/controllers/applicant_dashboard_controller.rb
@@ -1,3 +1,4 @@
+# app/controllers/applicant_dashboard_controller.rb
 class ApplicantDashboardController < ApplicationController
   def index
     @profile = current_user.profile
@@ -17,24 +18,34 @@ class ApplicantDashboardController < ApplicationController
     end
 
     @entries = Entry.active.where(profile: @profile)
+
+    respond_to do |format|
+      format.html
+      format.turbo_stream
+    end
   end
 
   private
 
   def available_contests
-    # @active_contests = ContestInstance.active_and_open
-    #                                   .for_class_level(@profile.class_level_id)
-    #                                   .joins(contest_description: :container)
-    #                                   .includes(contest_description: :container)
-    #                                   .order('containers.name ASC')
-    #                                   .distinct
     @active_contests = ContestInstance.active_and_open
-    .for_class_level(@profile.class_level_id)
-    .with_public_visibility
-    .includes(contest_description: { container: :visibility })
-    .order('containers.name ASC')
-    .distinct
+                                      .for_class_level(@profile.class_level_id)
+                                      .with_public_visibility
+                                      .includes(contest_description: { container: :visibility })
+                                      .order('containers.name ASC')
+                                      .distinct
 
+    # Apply filtering if container_id is provided
+    if params[:container_id].present?
+      @active_contests = @active_contests.where(contest_descriptions: { container_id: params[:container_id] })
+    end
+
+    if params[:department_id].present?
+      @active_contests = @active_contests.joins(contest_description: :container)
+                                         .where(containers: { department_id: params[:department_id] })
+    end
+
+    # Update @active_contests_by_container after filtering
     @active_contests_by_container = @active_contests.group_by do |contest|
       contest.contest_description.container
     end
