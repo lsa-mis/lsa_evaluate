efd52b143d1d85e2d6822b35f588962d1993ce71
rsmokeUM <rsmoke@umich.edu>
2024-10-08 17:28:45 -0400

    Refactor assignments controller to handle user assignments by UID

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,27 +1,49 @@
 class AssignmentsController < ApplicationController
+  include ActionView::RecordIdentifier
   before_action :set_container
 
   def create
-    @assignment = @container.assignments.build(assignment_params)
+    @user = User.find_by(uid: assignment_params[:uid])
 
-    if @assignment.save
-      @assignments = @container.assignments.includes(:user, :role)
+    if @user.nil?
+      @assignment = @container.assignments.build
+      @assignment.errors.add(:uid, 'User with this UID does not exist.')
       respond_to do |format|
-        format.turbo_stream
-        format.html { redirect_to @container, notice: 'Assignment was successfully created.' }
+        format.turbo_stream { render :create, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
       end
     else
-      render :new
+      @assignment = @container.assignments.build(user: @user, role_id: assignment_params[:role_id])
+
+      if @assignment.save
+        @assignments = @container.assignments.includes(:user, :role)
+        respond_to do |format|
+          format.turbo_stream
+          format.html { redirect_to @container, notice: 'Assignment was successfully created.' }
+        end
+      else
+        respond_to do |format|
+          format.turbo_stream { render :create, status: :unprocessable_entity }
+          format.html { render :new, status: :unprocessable_entity }
+        end
+      end
     end
   end
 
   def destroy
     @assignment = @container.assignments.find(params[:id])
-    @assignment.destroy
-
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to @container, notice: 'Assignment was successfully removed.' }
+  
+    if @assignment.destroy
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.remove(ActionView::RecordIdentifier.dom_id(@assignment)) }
+        format.html { redirect_to @container, notice: 'Assignment was successfully removed.' }
+      end
+    else
+      respond_to do |format|
+        # Replace the row content with the error message instead of removing it
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(ActionView::RecordIdentifier.dom_id(@assignment), partial: 'containers/assignment_row_with_error', locals: { assignment: @assignment, container: @container }) }
+        format.html { redirect_to @container, alert: @assignment.errors.full_messages.to_sentence }
+      end
     end
   end
 
@@ -32,6 +54,6 @@ class AssignmentsController < ApplicationController
   end
 
   def assignment_params
-    params.require(:assignment).permit(:user_id, :role_id)
+    params.require(:assignment).permit(:uid, :role_id)
   end
 end
