600fb5b263164d3430bf3dee0aac9eb60fcc623a
rsmokeUM <rsmoke@umich.edu>
2024-10-09 18:47:50 -0400

    Refactor render_editable_content method to include edit link for authorized users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def render_rich_text_without_outer_div(content)
-    raw content.body.to_html.gsub(%r{<div class="trix-content">(.*)</div>}m, '\1')
-  end
 
   def render_editable_content(page, section)
-    content = EditableContent.find_by(page:, section:)
-    return unless content
+    content_record = EditableContent.find_by(page: page, section: section)
+    return unless content_record
 
-    content_html = render_rich_text_without_outer_div(content.content)
+    # Convert the rich text content to an HTML-safe string
+    content_html = content_record.content.to_s
+
+    # Check if the current user has the `axis_mundi` role
     if current_user&.axis_mundi?
-      edit_link = link_to(edit_editable_content_path(content), class: 'edit-link ms-2') do
+      # Create the edit link with an icon
+      edit_link = link_to(edit_editable_content_path(content_record), class: 'edit-link ms-2', title: 'Edit') do
         content_tag(:i, '', class: 'bi bi-pencil')
       end
-      content_html += edit_link
+      # Combine the content and the edit link, marking it as HTML safe
+      safe_join([content_html, edit_link], ' ').html_safe
+    else
+      # If the user doesn't have the role, just render the content
+      content_html.html_safe
     end
-    content_html.html_safe
   end
 
   def format_datetime(datetime)
