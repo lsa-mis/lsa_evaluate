4329965b78e2ed21829d148ac110575fb215a9f5
rsmokeUM <rsmoke@umich.edu>
2024-10-13 17:06:18 -0400

    Refactor ProfilePolicy to simplify user ownership checks

--- app/policies/profile_policy.rb
+++ app/policies/profile_policy.rb
@@ -1,6 +1,6 @@
 class ProfilePolicy < ApplicationPolicy
   def index?
-    user_is_owner? || axis_mundi?
+    axis_mundi?
   end
 
   def new?
@@ -12,24 +12,24 @@ class ProfilePolicy < ApplicationPolicy
   end
 
   def show?
-    (user_has_persisted_profile? && user_is_owner?) || axis_mundi?
+    user_is_owner? || axis_mundi?
   end
 
   def update?
-    (user_has_persisted_profile? && user_is_owner?) || axis_mundi?
+    user_is_owner? || axis_mundi?
   end
 
   def destroy?
-    (user_has_persisted_profile? && user_is_owner?) || axis_mundi?
+    user_is_owner? || axis_mundi?
   end
 
   private
 
   def user_is_owner?
-    record.present? && record.user_id == user.id
+    user && record && record.user_id == user.id
   end
 
   def user_has_persisted_profile?
-    user.profile&.persisted?
+    user&.profile&.persisted?
   end
 end
