d5057e6414e28d9e34df17400d5598adff034002
rsmokeUM <rsmoke@umich.edu>
2024-10-16 14:40:49 -0400

    Refactor EntriesController to authorize index action

--- spec/requests/entries_spec.rb
+++ spec/requests/entries_spec.rb
@@ -1,3 +1,43 @@
+# spec/requests/entries_spec.rb
+require 'rails_helper'
+
+RSpec.describe "Entries", type: :request do
+  describe "GET /entries" do
+    context "as an axis_mundi user" do
+      let(:axis_mundi_user) { create(:user, :with_axis_mundi_role) } # Ensure you have a trait for axis_mundi
+
+      before do
+        sign_in axis_mundi_user
+      end
+
+      it "allows access to the index" do
+        get entries_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "as a normal authenticated user" do
+      let(:normal_user) { create(:user) }
+
+      before do
+        sign_in normal_user
+      end
+
+      it "denies access to the index" do
+        get entries_path
+        expect(flash[:alert]).to eq("You are not authorized to perform index? on Entry policy.")
+      end
+    end
+
+    context "as a guest (unauthenticated user)" do
+      it "redirects to the sign-in page" do
+        get entries_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+end
+
 # # spec/requests/entries_spec.rb
 # require 'rails_helper'
 
