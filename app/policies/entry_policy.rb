721c7c124f33c8a58fe5926bfb458e532afc8625
rsmokeUM <rsmoke@umich.edu>
2024-10-16 16:08:49 -0400

    Refactor EntryPolicy to remove unused show and update methods

--- app/policies/entry_policy.rb
+++ app/policies/entry_policy.rb
@@ -1,14 +1,6 @@
 class EntryPolicy < ApplicationPolicy
-  def show?
-    (record.profile.user == user && !record.deleted) || axis_mundi?
-  end
-
   def create?
     (record.profile.user == user && record.contest_instance.open?) || axis_mundi?
-  end
-
-  def update?
-    (record.profile.user == user && record.contest_instance.open? && !record.deleted) || axis_mundi?
   end
 
   def soft_delete?
