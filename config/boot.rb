7dcd960c9fbecd3b0fa1bbea68decb9f587855b6
rsmokeUM <rsmoke@umich.edu>
2024-11-14 17:07:44 -0500

    clean up development_feature setting

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,9 @@
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-ENV['RAILS_ENV'] = 'development_feature' if `git rev-parse --abbrev-ref HEAD`.chomp == 'evaluate_feature_branch'
+unless ENV['RAILS_ENV'] == 'production'
+  ENV['RAILS_ENV'] = 'development_feature' if `git rev-parse --abbrev-ref HEAD`.chomp == 'evaluate_feature_branch'
+end
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
