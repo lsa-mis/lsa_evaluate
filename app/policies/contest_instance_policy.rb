998ad36f0689125f24d83aae25bba84309ddaa97
rsmokeUM <rsmoke@umich.edu>
2024-11-18 15:37:57 -0500

    UPDATE policy related to viewing judging results

--- app/policies/contest_instance_policy.rb
+++ app/policies/contest_instance_policy.rb
@@ -22,4 +22,11 @@ class ContestInstancePolicy < ApplicationPolicy
   def manage_judges?
     user&.has_container_role?(record.contest_description.container) || axis_mundi?
   end
+
+  def view_judging_results?
+    return true if user&.axis_mundi?
+    return true if user&.has_container_role?(record.contest_description.container)
+    return true if record.judges.include?(user) && record.judge_evaluations_complete?
+    false
+  end
 end
