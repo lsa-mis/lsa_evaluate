b9e2ac686aaf60d6e7eaaf1417693632c253b8c8
rsmokeUM <rsmoke@umich.edu>
2024-12-02 14:16:02 -0500

    Add contest and round assignment checks in EntryRankingsController

--- app/controllers/entry_rankings_controller.rb
+++ app/controllers/entry_rankings_controller.rb
@@ -1,6 +1,8 @@
 class EntryRankingsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_judge
+  before_action :ensure_contest_assignment
+  before_action :ensure_round_assignment
   
   def create
     @entry_ranking = EntryRanking.find_or_initialize_by(
@@ -9,6 +11,7 @@ class EntryRankingsController < ApplicationController
       user_id: current_user.id
     )
     
+    authorize @entry_ranking
     @entry_ranking.rank = entry_ranking_params[:rank]
     
     if @entry_ranking.save
@@ -19,6 +22,23 @@ class EntryRankingsController < ApplicationController
   end
 
   private
+
+  def ensure_contest_assignment
+    contest_instance = JudgingRound.find(entry_ranking_params[:judging_round_id])
+                                 .contest_instance
+    
+    unless contest_instance.judge_assigned?(current_user)
+      redirect_to root_path, alert: 'You are not assigned to this contest'
+    end
+  end
+
+  def ensure_round_assignment
+    round = JudgingRound.find(entry_ranking_params[:judging_round_id])
+    
+    unless round.round_judge_assignments.active.exists?(user: current_user)
+      redirect_to root_path, alert: 'You are not assigned to this judging round'
+    end
+  end
 
   def entry_ranking_params
     params.require(:entry_ranking).permit(:entry_id, :judging_round_id, :rank)
