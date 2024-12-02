57daef14d5d948a85c9951e2dbebd7b882ebb283
rsmokeUM <rsmoke@umich.edu>
2024-12-02 15:05:42 -0500

    migrate

--- spec/factories/judging_rounds.rb
+++ spec/factories/judging_rounds.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: judging_rounds
+#
+#  id                  :bigint           not null, primary key
+#  active              :boolean          default(FALSE), not null
+#  completed           :boolean          default(FALSE), not null
+#  end_date            :datetime
+#  round_number        :integer          not null
+#  start_date          :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  contest_instance_id :bigint           not null
+#
+# Indexes
+#
+#  index_judging_rounds_on_contest_instance_id                   (contest_instance_id)
+#  index_judging_rounds_on_contest_instance_id_and_round_number  (contest_instance_id,round_number) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (contest_instance_id => contest_instances.id)
+#
 FactoryBot.define do
   factory :judging_round do
     contest_instance
