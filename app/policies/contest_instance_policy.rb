3a81d5425b68514d5568caf721e6efe6d8cd4c3d
rsmokeUM <rsmoke@umich.edu>
2025-01-08 18:42:04 -0500

    Enhance ContestInstancePolicy with new management and ranking methods
    - Added `manage?`, `update_rankings?`, `finalize_rankings?`, and `complete?` methods to improve access control and validation for contest instance management.
    - These methods ensure that only authorized users can manage and finalize rankings, enhancing the overall integrity of the judging process.

--- app/policies/contest_instance_policy.rb
+++ app/policies/contest_instance_policy.rb
@@ -29,4 +29,24 @@ class ContestInstancePolicy < ApplicationPolicy
     return true if record.judges.include?(user) && record.judge_evaluations_complete?
     false
   end
+
+  def manage?
+    user&.has_container_role?(record.contest_description.container) || axis_mundi?
+  end
+
+  def update_rankings?
+    return false unless user && record
+    return false unless record.judging_open?
+    record.judges.include?(user)
+  end
+
+  def finalize_rankings?
+    return false unless user && record
+    return false unless record.judging_open?
+    record.judges.include?(user)
+  end
+
+  def complete?
+    user&.has_container_role?(record.contest_description.container) || axis_mundi?
+  end
 end
