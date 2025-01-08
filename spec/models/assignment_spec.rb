1a21eb884b395386539749d89f5c4456495035ca
rsmokeUM <rsmoke@umich.edu>
2025-01-08 18:44:48 -0500

    Enhance assignment model specs with schema information
    - Added detailed schema information comments to the assignment model spec file, including table structure, indexes, and foreign key relationships.
    - This update improves documentation and clarity for developers working with the assignment model, ensuring better understanding of its database structure.

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: assignments
+#
+#  id           :bigint           not null, primary key
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  container_id :bigint           not null
+#  role_id      :bigint           not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_assignments_on_container_id              (container_id)
+#  index_assignments_on_role_id                   (role_id)
+#  index_assignments_on_role_user_container       (role_id,user_id,container_id) UNIQUE
+#  index_assignments_on_user_id                   (user_id)
+#  index_assignments_on_user_id_and_container_id  (user_id,container_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (container_id => containers.id)
+#  fk_rails_...  (role_id => roles.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Assignment, type: :model do
