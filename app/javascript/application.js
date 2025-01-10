c1ed27246d51970c8df36a8d5e172b26fae8c9f5
rsmokeUM <rsmoke@umich.edu>
2025-01-10 17:22:17 -0500

    Add Sortable.js for drag-and-drop functionality in application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,9 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import Sortable from "sortablejs"
 
 import "trix"
 import "@rails/actiontext"
+
+window.Sortable = Sortable;
