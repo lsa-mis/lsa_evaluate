13829adc780cf11f4ba1476c9dedf32933d7c1cd
rsmokeUM <rsmoke@umich.edu>
2025-01-29 11:04:41 -0500

    Extend ContestInstanceElement with drag-and-drop and entry card methods
    - Added methods to check if a contest instance is expanded
    - Implemented methods to interact with rated and available entries areas
    - Added functionality to drag entry cards between areas
    - Included a method to count entry cards

--- spec/support/page_objects/contest_instance_element.rb
+++ spec/support/page_objects/contest_instance_element.rb
@@ -8,5 +8,26 @@ module PageObjects
     def expand
       @page.find('.accordion-button', text: @contest_instance.contest_description.name).click
     end
+
+    def expanded?
+      @page.has_css?('.accordion-collapse.show')
+    end
+
+    def rated_entries_area
+      @page.find("[data-entry-drag-target='ratedEntries']")
+    end
+
+    def available_entries_area
+      @page.find("[data-entry-drag-target='availableEntries']")
+    end
+
+    def drag_entry_card_to_rated_entries_area(entry_card)
+      drag_handle = entry_card.find('.drag-handle')
+      drag_handle.drag_to(rated_entries_area)
+    end
+
+    def entry_card_count
+      @page.all('.card').count
+    end
   end
 end
