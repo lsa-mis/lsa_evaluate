d6192e93aa370c86ae70c16d1237957d346ff488
rsmokeUM <rsmoke@umich.edu>
2025-02-06 20:16:57 -0500

    Add ContainersHelper with contest description action links and eligibility rules rendering
    - Implemented `contest_description_action_links` method to generate action links for contest descriptions
    - Created `render_eligibility_rules` method to display and truncate eligibility rules with a modal link

---
+++ app/helpers/containers_helper.rb
@@ -0,0 +1,43 @@
+module ContainersHelper
+  def contest_description_action_links(container, description)
+    {
+      view: {
+        path: container_contest_description_contest_instances_path(container, description),
+        icon: 'eye',
+        title: 'View instances'
+      },
+      edit: {
+        path: edit_container_contest_description_path(container, description),
+        icon: 'pencil',
+        title: 'Edit contest'
+      },
+      archive: {
+        path: container_contest_description_path(container, description),
+        icon: 'archive',
+        title: 'Archive contest',
+        data: {
+          method: :delete,
+          controller: 'confirm',
+          confirm_message_value: 'Are you sure you want to archive this?'
+        }
+      }
+    }
+  end
+
+  def render_eligibility_rules(description)
+    eligibility_plain = description.eligibility_rules.to_plain_text
+    if eligibility_plain.length > 100
+      content_tag(:div) do
+        truncate(eligibility_plain, length: 100, omission: '') +
+        link_to('...more', '#',
+          data: {
+            action: 'click->eligibility-modal#open',
+            url: eligibility_rules_container_contest_description_path(description.container, description)
+          }
+        )
+      end
+    else
+      description.eligibility_rules
+    end
+  end
+end
