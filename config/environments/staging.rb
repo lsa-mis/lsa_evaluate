84f103e5cb83fcf09b21e6dc21b88cd10e8d3604
rsmokeUM <rsmoke@umich.edu>
2025-03-13 12:34:45 -0400

    Fix Action Mailer default URL options in staging environment configuration
    - Update the default_url_options for Action Mailer to correctly reference the host variable, ensuring proper email delivery in the staging environment.
    - This change enhances clarity and correctness in the configuration, aligning with Rails conventions.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Action Mailer
   host = 'https://evaluate-staging.lsa.umich.edu/'
-  config.action_mailer.default_url_options = { host: }
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :letter_opener_web
